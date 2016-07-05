645e21a5d57a3a13393d9a0c66f5b4a61b0fe8c6
Luke O'Neill <lukeo3210@gmail.com>
2016-07-05 11:58:21 -0400

    First commit

---
+++ Fuse Score/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Fuse Score
+//
+//  Created by Luke O'Neill on 7/5/16.
+//  Copyright © 2016 Luke O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var rowTextField: UITextField!
+    @IBOutlet weak var cycleTextField: UITextField!
+    @IBOutlet weak var skiTextField: UITextField!
+    @IBOutlet weak var totalTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func saveScore(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func previousScores(sender: AnyObject) {
+        
+    }
+
+}
+
